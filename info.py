4c065a76fa58ef27012eb0362dcc1e782b1a1623
Vikas-841 <150503522+Vikas-841@users.noreply.github.com>
2023-11-26 16:25:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,5 +3,5 @@ API_HASH     = "b486ee260537697fdfc56b2b61cbc049"
 BOT_TOKEN    = "6721499878:AAHlp2jWvhwOJpa2VezO1llYrAEoB5mhnaw"
 SESSION      = "BQGKEwAAnTJ-C83Jk92qOv0EhuA5NoBo-l0dtfLeXegyU4gYFhEhw3BZdGIaiFJo_IZM5xIjqdrACcxcShw1jpBIIwrqnjM67-962hmRaF1M-VDQtwj-jbhrDzDmib_BKFaFhhdCb7uBBHrKye0Yju1JJbHR_rs10PWKa0FbWybXnjaHQ_a3dpraReNNXFe5X69uMHfp1btQzCKw-n_0Gi3cs9-MneQFJ25polmHCG8LYoQHMdwwhBdzMWQc04KlXhCxKmt3dOBv1H5ZFVzdvPZlo64A51vAKQcM-sFToCSu1byf3h269AafzFGRxZiKpqT3Dg-F9b_SttmfUivPb_632eZXiQAAAAGId6yiAA"
 DATABASE_URI = "mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1002126379861
+LOG_CHANNEL  = -1001875834087
 ADMIN        = 6324330202
